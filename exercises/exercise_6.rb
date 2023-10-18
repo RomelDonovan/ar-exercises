69caedc1199ba1483f8f560b5644e01af099a0a8
RomelDonovan <romeldonovan@live.ca>
2023-10-18 11:49:37 -0400

    Add and create employees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,35 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many(:employees)
+Employee.belongs_to(:store)
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60
+  )
+@store1.employees.create(
+  first_name: "Romel", 
+  last_name: "Donovan", 
+  hourly_rate: 55
+  )
+@store2.employees.create(
+  first_name: "Ryan", 
+  last_name: "Schelhaas", 
+  hourly_rate: 50
+  )
+@store3.employees.create(
+  first_name: "Justin", 
+  last_name: "Hao", 
+  hourly_rate: 45
+  )
+@store4.employees.create(
+  first_name: "Fahad", 
+  last_name: "Kamal", 
+  hourly_rate: 40
+  )
+@store5.employees.create(
+  first_name: "Ralph", 
+  last_name: "D'Couto", 
+  hourly_rate: 35
+  )
