7784c45d320377593e05f8955d23d7bb81376346
RomelDonovan <romeldonovan@live.ca>
2023-10-18 14:09:41 -0400

    Validate employees, stores & create stores by user

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { in: 40..200 }
+end
+
+class Store < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality:{ in: 0 }
+end
+
+store_name = gets.chomp
+p Store.create(name: store_name)
